b2c73e1f439aae4acee506bedad3d7b6406e3213
Utkarsha <utkarsha.shukla@outlook.com>
2023-11-13 20:34:02 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='math_quiz',
-    version='0.1.0')
+    name='my_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [],
+    },
 )
